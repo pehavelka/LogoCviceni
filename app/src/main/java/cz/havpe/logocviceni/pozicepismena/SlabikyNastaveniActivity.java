eb344ca9522ab72ff8724d0c408237b74f9f69dd
Petr Havelka <pe.havelka@gmail.com>
2015-03-17 23:20:52 +0100

    slabiky

--- app/src/main/java/cz/havpe/logocviceni/pozicepismena/SlabikyNastaveniActivity.java
+++ app/src/main/java/cz/havpe/logocviceni/pozicepismena/SlabikyNastaveniActivity.java
@@ -1,13 +1,24 @@
 package cz.havpe.logocviceni.pozicepismena;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 import cz.havpe.logocviceni.R;
 
 public class SlabikyNastaveniActivity extends Activity {
+
+    ImageButton imageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,21 @@ public class SlabikyNastaveniActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_slabiky_nastaveni);
+
+        File image = new  File(Environment.getExternalStorageDirectory().toString() + "//LogoCviceni//Obrazky//Abeceda//had.png");
+        if(image.exists()){
+            imageBtn= (ImageButton) findViewById(R.id.imgTest);
+            imageBtn.setImageBitmap(BitmapFactory.decodeFile(image.getAbsolutePath()));
+
+            imageBtn.setOnClickListener(new View.OnClickListener() {
+                                                       @Override
+                                                       public void onClick(View v) {
+                                                           Toast.makeText(SlabikyNastaveniActivity.this, "Klik na hada.", Toast.LENGTH_LONG).show();
+                                                       }
+                                                   }
+
+            );
+        }
     }
 
 }
