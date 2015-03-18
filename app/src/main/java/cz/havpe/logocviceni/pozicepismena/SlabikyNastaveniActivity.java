2327ad36a5566b26f15eef22662a291913f09e38
Petr Havelka <pe.havelka@gmail.com>
2015-03-18 21:45:13 +0100

    slabiky

--- app/src/main/java/cz/havpe/logocviceni/pozicepismena/SlabikyNastaveniActivity.java
+++ app/src/main/java/cz/havpe/logocviceni/pozicepismena/SlabikyNastaveniActivity.java
@@ -3,6 +3,7 @@ package cz.havpe.logocviceni.pozicepismena;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -13,12 +14,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 
 import cz.havpe.logocviceni.R;
 
 public class SlabikyNastaveniActivity extends Activity {
 
     ImageButton imageBtn;
+    MediaPlayer player = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +34,34 @@ public class SlabikyNastaveniActivity extends Activity {
 
         setContentView(R.layout.activity_slabiky_nastaveni);
 
-        File image = new  File(Environment.getExternalStorageDirectory().toString() + "//LogoCviceni//Obrazky//Abeceda//had.png");
+        File image = new  File(Environment.getExternalStorageDirectory().toString() + "//LogoCviceni//Obrazky//Abeceda//čepice.png");
         if(image.exists()){
-            imageBtn= (ImageButton) findViewById(R.id.imgTest);
+            imageBtn = (ImageButton) findViewById(R.id.imgTest);
             imageBtn.setImageBitmap(BitmapFactory.decodeFile(image.getAbsolutePath()));
 
             imageBtn.setOnClickListener(new View.OnClickListener() {
                                                        @Override
                                                        public void onClick(View v) {
+                                                           _zvuk();
                                                            Toast.makeText(SlabikyNastaveniActivity.this, "Klik na hada.", Toast.LENGTH_LONG).show();
                                                        }
                                                    }
 
             );
         }
+
     }
 
+    private void _zvuk()  {
+        MediaPlayer mp = new MediaPlayer();
+        try {
+            mp.setDataSource(Environment.getExternalStorageDirectory().toString() + "//LogoCviceni//Zvuky//Abeceda//č.mp3");
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
